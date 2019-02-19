480e1bdf94965dfdf6e482a2adc4a10c524c6097
Alyx Malbrough <amalbrou@asu.edu>
2019-02-18 19:40:49 -0700

    added VS files with logon/logoff functionality

---
+++ project1/ProjectServices.asmx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+// need these to talk to mysql
+using MySql.Data;
+using MySql.Data.MySqlClient;
+// need this to manipulate data from a database
+using System.Data;
+
+namespace project1
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        // test method to make sure everything is working! can be commented out later :)
+        [WebMethod]
+        public int NumberofAccounts()
+        {
+            // grab the connection string from the web.config file
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            // the query to run on the database
+            string sqlSelect = "SELECT * from accounts";
+
+            // connection object
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            // command object uses connection and query
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            // data adapter is a bridge between command and data we're wanting back
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            // table we want to fill with query results
+            DataTable sqlDt = new DataTable();
+            // filling the table
+            sqlDa.Fill(sqlDt);
+            // return number of rows
+            return sqlDt.Rows.Count;
+        }
+
+        //EXAMPLE OF A SIMPLE SELECT QUERY (PARAMETERS PASSED IN FROM CLIENT)
+        [WebMethod(EnableSession = true)] //NOTICE: gotta enable session on each individual method
+        public bool LogOn(string uid, string pass)
+        {
+            //we return this flag to tell them if they logged in or not
+            bool success = false;
+
+            //our connection string comes from our web.config file like we talked about earlier
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //here's our query.  A basic select with nothing fancy.  Note the parameters that begin with @
+            //NOTICE: we added admin to what we pull, so that we can store it along with the id in the session
+            string sqlSelect = "SELECT Id, Admin FROM accounts WHERE Username=@idValue and Password=@passValue";
+
+            //set up our connection object to be ready to use our connection string
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            //set up our command object to use our connection, and our query
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //tell our command to replace the @parameters with real values
+            //we decode them because they came to us via the web so they were encoded
+            //for transmission (funky characters escaped, mostly)
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+            sqlCommand.Parameters.AddWithValue("@passValue", HttpUtility.UrlDecode(pass));
+
+            //a data adapter acts like a bridge between our command object and 
+            //the data we are trying to get back and put in a table object
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            //here's the table we want to fill with the results from our query
+            DataTable sqlDt = new DataTable();
+            //here we go filling it!
+            sqlDa.Fill(sqlDt);
+            //check to see if any rows were returned.  If they were, it means it's 
+            //a legit account
+            if (sqlDt.Rows.Count > 0)
+            {
+                //if we found an account, store the id and admin status in the session
+                //so we can check those values later on other method calls to see if they 
+                //are 1) logged in at all, and 2) and admin or not
+                Session["id"] = sqlDt.Rows[0]["id"];
+                Session["admin"] = sqlDt.Rows[0]["admin"];
+                success = true;
+            }
+            //return the result!
+            return success;
+        }
+
+
+        [WebMethod(EnableSession = true)]
+        public bool LogOff()
+        {
+            //if they log off, then we remove the session.  That way, if they access
+            //again later they have to log back on in order for their ID to be back
+            //in the session!
+            Session.Abandon();
+            return true;
+        }
+    }
+}
