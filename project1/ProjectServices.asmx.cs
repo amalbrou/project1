7aae01ccb1f99ad9da8625b00bf09e0f9194d7f6
David Olczak <dolczak@asu.edu>
2019-02-21 10:50:19 -0700

    Create a class will create an entry for the classes table, but UID is not properly being assigned

--- project1/ProjectServices.asmx.cs
+++ project1/ProjectServices.asmx.cs
@@ -143,5 +143,44 @@ namespace project1
             }
             sqlConnection.Close();
         }
+
+        [WebMethod(EnableSession = true)]
+        public void RequestClass(string uid, string professor, string days, string time, string comments)
+        {
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlSelect = "insert into classes (ClassName, Professor, Days, Time, Comments) " +
+                "values(@idValue, @profValue, @dayValue, @timeValue, @comValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+            sqlCommand.Parameters.AddWithValue("@profValue", HttpUtility.UrlDecode(professor));
+            sqlCommand.Parameters.AddWithValue("@dayValue", HttpUtility.UrlDecode(days));
+            sqlCommand.Parameters.AddWithValue("@timeValue", HttpUtility.UrlDecode(time));
+            sqlCommand.Parameters.AddWithValue("@comValue", HttpUtility.UrlDecode(comments));
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
     }
 }
