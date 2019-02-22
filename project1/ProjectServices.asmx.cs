9b22b767727cb4dcbb5ff54f5e0cba1a39e600f7
David Olczak <dolczak@asu.edu>
2019-02-22 16:05:47 -0700

    Create an assignment works within web services, but displays an error when ran through the HTML

--- project1/ProjectServices.asmx.cs
+++ project1/ProjectServices.asmx.cs
@@ -182,5 +182,44 @@ namespace project1
             }
             sqlConnection.Close();
         }
+
+        [WebMethod(EnableSession = true)]
+        public void RequestAssignment(string uid, string date, string time, string comments, string className)
+        {
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlSelect = "insert into assignments (AssignmentName, DueDate, DueTime, Comments, ClassName) " +
+                "values(@idValue, @dateValue, @timeValue, @comValue, @classValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(uid));
+            sqlCommand.Parameters.AddWithValue("@dateValue", HttpUtility.UrlDecode(date));
+            sqlCommand.Parameters.AddWithValue("@timeValue", HttpUtility.UrlDecode(time));
+            sqlCommand.Parameters.AddWithValue("@comValue", HttpUtility.UrlDecode(comments));
+            sqlCommand.Parameters.AddWithValue("@classValue", HttpUtility.UrlDecode(className));
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
     }
 }
